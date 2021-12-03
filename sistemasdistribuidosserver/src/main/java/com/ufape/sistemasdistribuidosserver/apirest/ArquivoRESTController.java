d11b557eabaa782ae298da95d2f5875314c18b65
Rafael Cavalcanti <rafael240519@gmail.com>
2021-12-03 17:21:17 -0300

    Add files via upload

--- sistemasdistribuidosserver/src/main/java/com/ufape/sistemasdistribuidosserver/apirest/ArquivoRESTController.java
+++ sistemasdistribuidosserver/src/main/java/com/ufape/sistemasdistribuidosserver/apirest/ArquivoRESTController.java
@@ -5,6 +5,7 @@ import java.util.Base64;
 import java.util.List;
 
 import com.ufape.sistemasdistribuidosserver.model.Arquivo;
+import com.google.gson.Gson;
 import com.ufape.sistemasdistribuidos.model.ArquivoAux;
 import com.ufape.sistemasdistribuidosserver.model.ArquivosCompartilhadosUsuarios;
 import com.ufape.sistemasdistribuidosserver.model.Requisicao;
@@ -17,6 +18,7 @@ import org.apache.commons.lang3.SerializationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -55,19 +56,15 @@ public class ArquivoRESTController {
         return SerializationUtils.serialize(arquivo);
     }
 
-    @PostMapping("/confimarRecebimento")
+    @PostMapping("/confimarRecebimento/{idUsuario}/{idArquivo}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Arquivo> confimarRecebimento(@RequestBody String ids) {
-        String[] id = ids.split("_");
-        Long idUsuario = Long.parseLong(id[0]);
-        Long idArquivo = Long.parseLong(id[1]);
-
+    public ResponseEntity<Arquivo> confimarRecebimento(@PathVariable("idUsuario") Long idUsuario, @PathVariable("idArquivo") Long idArquivo) {
         Requisicao r = requisicoesDAOI.findByIdsUsuarioArquivo(idUsuario, idArquivo);
         r.setAtivo(false);
 
         requisicoesDAOI.save(r);
 
-        return null;
+        return new ResponseEntity<Arquivo>(HttpStatus.OK);
     }
 
     @PostMapping("/novo")
@@ -99,13 +96,42 @@ public class ArquivoRESTController {
         return novoArquivo;
     }
 
-    @PostMapping
+    @PostMapping("/enviar")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Arquivo> enviar(@RequestBody byte[] arquivo) throws ClassNotFoundException, IOException {
         
     	ArquivoAux nArquivo = SerializationUtils.deserialize(arquivo);
         Arquivo novoArquivo = new Arquivo();
-        novoArquivo.setId(nArquivo.getId());
+        novoArquivo.setIdUsuario(nArquivo.getIdUsuario());
+        novoArquivo.setNome(nArquivo.getNome());
+        novoArquivo.setConteudo(nArquivo.getConteudo());
+        
+        List<Long> usuarios = usuarioDAOI.obterUsuarios(novoArquivo.getIdUsuario());
+        arquivoDAOI.save(novoArquivo);
+        for (int i=0; i<usuarios.size(); i++) {
+            Requisicao r = new Requisicao();
+            r.setId(10000000L);
+            r.setIdUsuario(usuarios.get(i));
+            r.setIdArquivo(novoArquivo.getId());
+            r.setTipoRequisicao(0L);
+            requisicoesDAOI.save(r);
+            
+            ArquivosCompartilhadosUsuarios acu = new ArquivosCompartilhadosUsuarios();
+            acu.setId(10000000L);
+            acu.setIdUsuario(usuarios.get(i));
+            acu.setIdArquivo(novoArquivo.getId());
+            arquivosCompartilhadosUsuariosDAOI.save(acu);
+        }
+        return new ResponseEntity<Arquivo>(novoArquivo, HttpStatus.OK);
+    }
+
+    @PostMapping("/enviarJson")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Arquivo> enviar(@RequestBody String arquivo) throws ClassNotFoundException, IOException {
+        
+        Gson gson = new Gson();
+        ArquivoAux nArquivo = gson.fromJson(arquivo, ArquivoAux.class);
+        Arquivo novoArquivo = new Arquivo();
         novoArquivo.setIdUsuario(nArquivo.getIdUsuario());
         novoArquivo.setNome(nArquivo.getNome());
         novoArquivo.setConteudo(nArquivo.getConteudo());
@@ -139,7 +165,11 @@ public class ArquivoRESTController {
         return arquivos;
     }
 
-
-    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Arquivo> deletarArquivo(@PathVariable("id") Long id) {
+        Arquivo arquivo = arquivoDAOI.getById(id);
+        arquivoDAOI.delete(arquivo);
+        return new ResponseEntity<Arquivo>(HttpStatus.OK);
+    }
 
 }
