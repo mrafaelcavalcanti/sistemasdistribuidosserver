91ede17f237deb0b7458eb69779ad31274d4c56a
Rafael Cavalcanti <rafael240519@gmail.com>
2021-12-03 21:02:33 -0300

    alterando requisicao para pegar apenas quando a requisicao estiver ativa

--- sistemasdistribuidosserver/src/main/java/com/ufape/sistemasdistribuidosserver/repository/RequisicoesDAOI.java
+++ sistemasdistribuidosserver/src/main/java/com/ufape/sistemasdistribuidosserver/repository/RequisicoesDAOI.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RequisicoesDAOI extends JpaRepository<Requisicao, Long> {
     
-    @Query("SELECT r FROM Requisicao r WHERE r.idUsuario=:id")
+    @Query("SELECT r FROM Requisicao r WHERE r.idUsuario=:id and ativo is not false")
     public List<Requisicao> findByUserId(Long id);
 
     @Query("SELECT r FROM ArquivosCompartilhadosUsuarios r Where r.idUsuario=:idUsuario AND r.idArquivo=:idArquivo")
