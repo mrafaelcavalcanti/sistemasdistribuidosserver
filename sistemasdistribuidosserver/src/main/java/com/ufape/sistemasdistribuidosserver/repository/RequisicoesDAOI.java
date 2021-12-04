d90e264648ab96bd47ece93ea106564b7958cf51
Rafael Cavalcanti <rafael240519@gmail.com>
2021-12-03 21:03:18 -0300

    consertando requisicao

--- sistemasdistribuidosserver/src/main/java/com/ufape/sistemasdistribuidosserver/repository/RequisicoesDAOI.java
+++ sistemasdistribuidosserver/src/main/java/com/ufape/sistemasdistribuidosserver/repository/RequisicoesDAOI.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RequisicoesDAOI extends JpaRepository<Requisicao, Long> {
     
-    @Query("SELECT r FROM Requisicao r WHERE r.idUsuario=:id and ativo is not false")
+    @Query("SELECT r FROM Requisicao r WHERE r.idUsuario=:id AND ativo IS NOT FALSE")
     public List<Requisicao> findByUserId(Long id);
 
     @Query("SELECT r FROM ArquivosCompartilhadosUsuarios r Where r.idUsuario=:idUsuario AND r.idArquivo=:idArquivo")
